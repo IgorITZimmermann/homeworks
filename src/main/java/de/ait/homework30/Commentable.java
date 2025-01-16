5691fcb579b80d306a7765abb10ba1ea51b8aed8
Igor Zimmermann <igor.sonstnix@gmail.com>
2025-01-17 00:24:16 +0100

    Add user, social media, and athlete management modules

---
+++ src/main/java/de/ait/homework30/Commentable.java
@@ -0,0 +1,36 @@
+package de.ait.homework30;
+
+import java.util.List;
+
+/**
+ * This interface represents a contract for objects that can manage comments.
+ * Objects implementing this interface are capable of adding and retrieving comments.
+ *
+ * Этот интерфейс задаёт контракт для объектов, способных управлять комментариями.
+ * Объекты, реализующие данный интерфейс, могут добавлять и получать комментарии.
+ */
+public interface Commentable {
+
+    /**
+     * Adds a comment to the object.
+     *
+     * @param comment the text of the comment to be added
+     *
+     * Добавляет комментарий к объекту.
+     *
+     * @param comment текст комментария, который будет добавлен
+     */
+    void addComment(String comment);
+
+    /**
+     * Retrieves the list of all comments.
+     *
+     * @return a list of comments
+     *
+     * Возвращает список всех комментариев.
+     *
+     * @return список комментариев
+     */
+    List<String> getComments();
+
+}
--- src/main/java/de/ait/interfaces/SocialAuth.java
+++ src/main/java/de/ait/interfaces/SocialAuth.java
@@ -14,6 +14,7 @@ public interface SocialAuth {
     // Константа для максимального количества попыток входа
     int MAX_LOGIN_ATTEMPTS = 3;
 
+
     /**
      * Authenticates a user using their credentials.
      *
