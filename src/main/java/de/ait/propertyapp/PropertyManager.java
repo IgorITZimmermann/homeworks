d266ce11a7c16df216eae7b97ca898995ff9a7ff
Igor Zimmermann <igor.sonstnix@gmail.com>
2025-01-22 10:34:09 +0100

    Add initial implementation of mail and property management

---
+++ src/main/java/de/ait/propertyapp/PropertyManager.java
@@ -0,0 +1,7 @@
+package de.ait.propertyapp;
+
+public class PropertyManager {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/de/ait/propertyapp/RealEstateAgency.java
@@ -0,0 +1,4 @@
+package de.ait.propertyapp;
+
+public class RealEstateAgency {
+}
