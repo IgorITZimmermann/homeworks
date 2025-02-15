9e620f6f716798af04d83491b04d26022f098908
Igor Zimmermann <igor.sonstnix@gmail.com>
2025-02-15 09:59:24 +0100

    Add initial implementation of YouTubeAnalyzer, CityApp, and Calculator classes

---
+++ src/main/java/de/ait/homework38/Operation.java
@@ -0,0 +1,18 @@
+package de.ait.homework38;
+
+/**
+ * Перечисление, представляющее арифметические операции.
+ * ADD - сложение
+ * SUBTRACT - вычитание
+ * MULTIPLY - умножение
+ * DIVIDE - деление
+ *
+ * Enum representing arithmetic operations.
+ * ADD - addition
+ * SUBTRACT - subtraction
+ * MULTIPLY - multiplication
+ * DIVIDE - division
+ */
+public enum Operation {
+    ADD, SUBTRACT, MULTIPLY, DIVIDE
+}
